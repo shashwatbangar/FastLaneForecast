bd4a1e195daedbba97f5ed7eac32f784444d1063
shashwatbangar <shashwatbangar@gmail.com>
2023-08-20 19:32:43 +0530

    Added: Intial files for this project

---
+++ includes/configuration.py
@@ -0,0 +1,3 @@
+raw_folder_path = 'hdfs://localhost:9000/user/sunbeam/FastLaneForecast/blob_containers/raw'
+processed_folder_path = 'hdfs://localhost:9000/user/sunbeam/FastLaneForecast/blob_containers/processed'
+presentation_folder_path = 'hdfs://localhost:9000/user/sunbeam/FastLaneForecast/blob_containers/presentation'
