278b4fd40397cfd7c71a56b5027edb168833a1ed
Shashwat Bangar <62360781+shashwatbangar@users.noreply.github.com>
2023-08-29 00:34:25 +0530

    Delete includes directory

--- includes/configuration.py
+++
@@ -1,3 +0,0 @@
-raw_folder_path = 'hdfs://localhost:9000/user/sunbeam/FastLaneForecast/blob_containers/raw'
-processed_folder_path = 'hdfs://localhost:9000/user/sunbeam/FastLaneForecast/blob_containers/processed'
-presentation_folder_path = 'hdfs://localhost:9000/user/sunbeam/FastLaneForecast/blob_containers/presentation'
